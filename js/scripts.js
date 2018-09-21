28285550556b2f89d6a1b4824ff025f1e9e52b37
nignanthomas <nignanthomas@gmail.com>
2018-09-21 17:05:54 +0300

    add the script for the business logic aka back-end

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// for radio buttons; value="1" for correct, value="9" for wrong
+
+
+//BUSINESS LOGIC : DRY CODE
+var arrayUser = [];
+var score = 0;
+var calculate = function(){
+  for (i = 1; i <= 5; i++) {
+    var response = $("input:radio[name=js"+i+"]:checked").val();
+    arrayUser.push(response);
+    if (arrayUser[i-1] == "1") {
+      score += 20;
+    }
+  }
+}
+// END BUSINESS LOGIC
