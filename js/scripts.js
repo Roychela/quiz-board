dcd26701764c8c2cffe73bdec8dc7fb25d00ef47
nignanthomas <nignanthomas@gmail.com>
2018-09-21 18:58:01 +0300

    add control for all the questions to be answered and display an appropriate remark

--- js/scripts.js
+++ js/scripts.js
@@ -4,22 +4,48 @@
 //BUSINESS LOGIC : DRY CODE
 var arrayUser = [];
 var score = 0;
+var complete = 0;
 var calculate = function(){
   for (i = 1; i <= 5; i++) {
+
     var response = $("input:radio[name=js"+i+"]:checked").val();
     arrayUser.push(response);
     if (arrayUser[i-1] == "1") {
       score += 20;
+    } // end if
+    if (arrayUser[i-1] !== undefined) {
+      complete += 1
     }
-  }
+  } // end for
+
 }
 // END BUSINESS LOGIC
 
 // USER INTERFACE
 $(document).ready(function(){
   $("form").submit(function(event){
+    event.preventDefault();
     calculate();
-    $("#display").text(score);
+    var message = ""
+    var remark =""
+    if (complete === 5) {
+      message = "Your score is:";
+      $("#display").text(score);
+      if (score <= 20) {
+        remark = "Bad";
+      } else if (score <= 60) {
+        remark = "Average";
+      } else if (score <= 80) {
+        remark = "Good";
+      } else {
+        remark ="Excellent";
+      }
+    } else {
+      message = "Please answer all the questions and submit again!";
+    }
+    $("#message").text(message);
+    $("#remark").text(remark);
+
   }) // end submit
 
 }) //END USER INTERFACE
